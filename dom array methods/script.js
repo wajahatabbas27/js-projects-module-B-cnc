786d7fd6cf57c7ff5d4d2fe78908ce1a049e4f40
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2020-10-10 17:39:12 +0500

    html css completed
    index.html,style.css completed

---
+++ dom array methods/script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleMoneyButton = document.getElementById('double');
+const showMillionairesButton = document.getElementById('show-millionaires');
+const sortButton = document.getElementById('sort');
+const totalButton = document.getElementById('calculate-total');
